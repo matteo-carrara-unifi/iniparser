94459e7f7ade81a7faf6a66579935786fb5e5bde
matteo-carrara-unifi <matteo.carrara@stud.unifi.it>
2021-07-14 18:29:18 +0200

    Updated test

--- test/INIFileFixture.cpp
+++ test/INIFileFixture.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <filesystem>
+#include <vector>
+#include <string>
 
 #include "gtest/gtest.h"
 #include "../src/INIFile.h"
@@ -31,15 +33,33 @@ TEST_F(INIFileSuite, TestOpen) {
 }
 
 TEST_F(INIFileSuite, TestGetSection) {
+    vector<string> correctSections = {"", "database", "this is another section", "this is an empty section", "this section is not empty"};
+
+    int i = 0;
     for(auto &s: myfile.getSections()) {
-        cout << "Section name is " << s.getName() << endl;
-        for (auto &p: s.getProp()) {
-            cout << "Property name: " << p.getName() << endl << "Property value: " << p.getValue() << endl << endl;
-        }
+        ASSERT_EQ(s.getName(), correctSections[i++]);
     }
 }
 
-TEST_F(INIFileSuite, TestAddSection) {
-    myfile.addSection("NEW SECTION 123").addKey("random key", "lulz value");
-    myfile["database"].addKey("New database key", "New database data");
+TEST_F(INIFileSuite, TestWrite) {
+    const string name = "NEW SECTION 123";
+    auto s1 = myfile.addSection(name);
+
+    EXPECT_NO_THROW({
+        myfile[name];
+    });
+
+    s1.addKey("random key", "lulz value");
+    bool keyfound = false;
+    for(auto &k: s1.getProp()) {
+        if(k.getName() == "random key") {
+            keyfound = true;
+            break;
+        }
+    }
+    ASSERT_TRUE(keyfound);
+
+    EXPECT_NO_THROW({
+        myfile["database"].addKey("New database key", "New database data");
+    });
 }
